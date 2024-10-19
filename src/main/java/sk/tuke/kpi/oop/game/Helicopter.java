a6f4c86e12b87fff16ea060ec28296b6902244d4
Danylo Senchyshyn <danylo.senchyshyn@student.tuke.sk>
2024-10-19 12:56:38 +0200

    cvik 4 12:56

--- src/main/java/sk/tuke/kpi/oop/game/Helicopter.java
+++ src/main/java/sk/tuke/kpi/oop/game/Helicopter.java
@@ -1,6 +1,7 @@
 package sk.tuke.kpi.oop.game;
 
 import sk.tuke.kpi.gamelib.actions.Invoke;
+import sk.tuke.kpi.gamelib.Scene;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.framework.Player;
 import sk.tuke.kpi.gamelib.framework.actions.Loop;
@@ -8,11 +9,18 @@ import sk.tuke.kpi.gamelib.graphics.Animation;
 
 public class Helicopter extends AbstractActor {
     private int newPositionX = 0, newPositionY = 0;
-    private Animation helicopterAnimation;
+    private Animation animationDown;
+    private Animation animationUp;
+    private Animation animationLeft;
+    private Animation animationRight;
 
     public Helicopter() {
-        helicopterAnimation = new Animation("sprites/heli.png", 64, 64, 0.2f, Animation.PlayMode.LOOP_PINGPONG);
-        setAnimation(helicopterAnimation);
+        animationDown = new Animation("sprites/heli_down.png", 64, 64, 0.2f, Animation.PlayMode.LOOP_PINGPONG);
+        animationUp = new Animation("sprites/heli_up.png", 64, 64, 0.2f, Animation.PlayMode.LOOP_PINGPONG);
+        animationLeft = new Animation("sprites/heli_left.png", 64, 64, 0.2f, Animation.PlayMode.LOOP_PINGPONG);
+        animationRight = new Animation("sprites/heli_right.png", 64, 64, 0.2f, Animation.PlayMode.LOOP_PINGPONG);
+
+        setAnimation(animationUp);
     }
 
     public void searchAndDestroy() {
@@ -23,26 +31,31 @@ public class Helicopter extends AbstractActor {
         Player refActor = getScene().getLastActorByType(Player.class);
         if (refActor == null) return;
 
-        if (this.getPosX() != refActor.getPosX()) {
-            if(this.getPosX() > refActor.getPosX()) {
-                newPositionX = this.getPosX()- 1;
-            } else {
-                newPositionX = this.getPosX() + 1;
-            }
-        }
+        int deltaX = (int) Math.signum(refActor.getPosX() - this.getPosX());
+        int deltaY = (int) Math.signum(refActor.getPosY() - this.getPosY());
 
-        if (this.getPosY() != refActor.getPosY()) {
-            if(this.getPosY() > refActor.getPosY()) {
-                newPositionY = this.getPosY()- 1;
-            } else {
-                newPositionY = this.getPosY() + 1;
-            }
-        }
+        newPositionX = this.getPosX() + deltaX;
+        newPositionY = this.getPosY() + deltaY;
 
         this.setPosition(newPositionX, newPositionY);
 
+        updateAnimationHelicopter(deltaX, deltaY);
+
         if (intersects(refActor)) {
-            refActor.setEnergy(refActor.getEnergy()-1);
+            refActor.setEnergy(refActor.getEnergy() - 1);
         }
+    }
+
+    public void updateAnimationHelicopter(int deltaX, int deltaY) {
+        if (deltaX > 0) {
+            setAnimation(animationRight);
+        } else if (deltaX < 0) {
+            setAnimation(animationLeft);
+        } else if (deltaY > 0) {
+            setAnimation(animationDown);
+        } else if (deltaY < 0) {
+            setAnimation(animationUp);
+        }
+
     }
 }
