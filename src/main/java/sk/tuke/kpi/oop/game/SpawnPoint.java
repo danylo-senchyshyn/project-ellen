3032d5e39903eeb2fe974a73d0e845c4e5ac0e10
Danylo Senchyshyn <danylo.senchyshyn@student.tuke.sk>
2024-12-06 21:25:50 +0100

    fix arena

--- src/main/java/sk/tuke/kpi/oop/game/SpawnPoint.java
+++ src/main/java/sk/tuke/kpi/oop/game/SpawnPoint.java
@@ -37,25 +37,21 @@ public class SpawnPoint extends AbstractActor {
         Scene scene = getScene();
         Actor ripley = getScene().getFirstActorByType(Ripley.class);
 
-        if (scene == null || remainingEnemies <= 0 || !isRipleyNear(ripley)) return;
+        if (ripley == null || scene == null) {
+            return;
+        } else if (remainingEnemies <= 0) {
+            dispose();
+            return;
+        }
 
-        Alien alien = new Alien(100, new RandomlyMoving());
-        scene.addActor(alien, getPosX() + getWidth() / 2, getPosY() + getHeight() / 2);
+        int dx = ripley.getPosX() + ripley.getWidth() / 2 - getPosX() - getWidth() / 2;
+        int dy = ripley.getPosY() + ripley.getHeight() / 2 - getPosY() - getHeight() / 2;
 
-        remainingEnemies--;
-
-        //System.out.println("spawn" + LocalDateTime.now());
-    }
-
-    private boolean isRipleyNear(Actor actor) {
-        Actor ripley = getScene().getFirstActorByType(Ripley.class);
-        if (ripley == null) return false;
-
-        int dx = actor.getPosX() + actor.getWidth() / 2 - getPosX() - getWidth() / 2;
-        int dy = actor.getPosY() + actor.getHeight() / 2 - getPosY() - getHeight() / 2;
-        double distance = Math.sqrt(dx * dx + dy * dy);
-
-        return distance <= 50;
+        if (dx <= 50 && dy <= 50) {
+            Alien alien = new Alien(100, new RandomlyMoving());
+            scene.addActor(alien, getPosX() + getWidth() / 2, getPosY() + getHeight() / 2);
+            remainingEnemies--;
+        }
     }
 
     public void dispose() {
